67ea5369e3d6074645ea43a96f8750dfbe5f9f52
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-25 15:26:41 +1200

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+drop table if exists "user";
+
+
+create table "user"
+(
+
+    id       INT auto_increment PRIMARY KEY,
+    username VARCHAR(32) NOT NULL,
+    password VARCHAR(32) NOT NULL
+
+);
